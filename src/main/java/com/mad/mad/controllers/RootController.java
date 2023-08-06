97f1e8b59a43e20ec579ccde3bc3f3d449d2b1b8
maaaaaaaaad <integral5137@gmail.com>
2023-08-06 19:27:46 +0900

    e

--- src/main/java/com/mad/mad/controllers/RootController.java
+++ src/main/java/com/mad/mad/controllers/RootController.java
@@ -2,12 +2,30 @@ package com.mad.mad.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
- public class RootController {
+public class RootController {
+
+    static class Welcome {
+        private String name;
+
+        public String getName() {
+            String greeting_message = "hello";
+            return greeting_message + " " + this.name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
     @GetMapping("/")
-    public String init(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
+    @ResponseBody
+    public String init(@RequestParam("name") String name) {
+        Welcome welcome = new Welcome();
+        welcome.setName(name);
+        return welcome.getName();
     }
 }
