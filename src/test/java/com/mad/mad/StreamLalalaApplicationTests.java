302582764c53aff239187798ff5091ddc8a152d2
maaaaaaaaad <integral5137@gmail.com>
2023-08-06 16:50:58 +0900

    e

---
+++ src/test/java/com/mad/mad/StreamLalalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mad.mad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamLalalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
