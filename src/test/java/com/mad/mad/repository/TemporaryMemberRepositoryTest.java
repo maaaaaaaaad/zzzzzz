0a0f552baea264ebc10892849d297d3408e5fffa
maaaaaaaaad <integral5137@gmail.com>
2023-08-07 21:19:42 +0900

    e

--- src/test/java/com/mad/mad/repository/TemporaryMemberRepositoryTest.java
+++ src/test/java/com/mad/mad/repository/TemporaryMemberRepositoryTest.java
@@ -4,10 +4,11 @@ package com.mad.mad.repository;
 import com.mad.mad.domain.entity.Member;
 import com.mad.mad.domain.repository.MemberRepository;
 import com.mad.mad.domain.repository.TemporaryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class TemporaryMemberRepositoryTest {
     final String testName = "mad";
@@ -19,21 +20,18 @@ public class TemporaryMemberRepositoryTest {
         this.member.setName(this.testName);
         repository.save(this.member);
         Member resultById = repository.findById(this.member.getId()).get();
-        Assertions.assertThat(this.member).isEqualTo(resultById);
-        Assertions.assertThat(resultById.getId()).isEqualTo(1L);
-        Assertions.assertThat(resultById.getId()).isNotZero();
+        assertThat(this.member).isEqualTo(resultById);
+        assertThat(resultById.getId()).isEqualTo(1L);
+        assertThat(resultById.getId()).isNotZero();
         Member resultByName = repository.findByName(this.member.getName()).get();
-        Assertions.assertThat(this.member).isEqualTo(resultByName);
-        Assertions.assertThat(resultByName.getName()).isEqualTo(this.testName);
-        Assertions.assertThat(resultByName.getName()).isNotEmpty();
+        assertThat(this.member).isEqualTo(resultByName);
+        assertThat(resultByName.getName()).isEqualTo(this.testName);
+        assertThat(resultByName.getName()).isNotEmpty();
     }
 
     @Test()
     public void findById() {
         Optional<Member> empty = repository.findById(1000L);
-        Assertions.assertThat(empty).isEmpty();
-        this.save();
-        Member result = repository.findById(this.member.getId()).get();
-        Assertions.assertThat(result.getId()).isEqualTo(this.member.getId());
+        assertThat(empty).isEmpty();
     }
 }
