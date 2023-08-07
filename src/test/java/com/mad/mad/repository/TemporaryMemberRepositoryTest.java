f6fa8ab8102be34d72897ed28f542a55ce6de62d
maaaaaaaaad <integral5137@gmail.com>
2023-08-07 21:00:22 +0900

    e

--- src/test/java/com/mad/mad/repository/TemporaryMemberRepositoryTest.java
+++ src/test/java/com/mad/mad/repository/TemporaryMemberRepositoryTest.java
@@ -10,17 +10,24 @@ import org.junit.jupiter.api.Test;
 public class TemporaryMemberRepositoryTest {
 
     final String testName = "mad";
+    final Long testId = 1000L;
 
     MemberRepository repository = new TemporaryMemberRepository();
+    Member member = new Member();
 
     @Test()
     public void save() {
-        Member member = new Member();
-        member.setName(this.testName);
-        repository.save(member);
-        Member result = repository.findByName(member.getName()).get();
-        Assertions.assertThat(member).isEqualTo(result);
-        Assertions.assertThat(result.getName()).isEqualTo(this.testName);
-        Assertions.assertThat(result.getName()).isNotEmpty();
+        this.member.setId(this.testId);
+        this.member.setName(this.testName);
+        repository.save(this.member);
+        Member resultById = repository.findByName(this.member.getName()).get();
+        Assertions.assertThat(this.member).isEqualTo(resultById);
+        System.out.print(resultById.getId());
+        Assertions.assertThat(resultById.getId()).isEqualTo(1L);
+        Assertions.assertThat(resultById.getId()).isNotZero();
+        Member resultByName = repository.findByName(this.member.getName()).get();
+        Assertions.assertThat(this.member).isEqualTo(resultByName);
+        Assertions.assertThat(resultByName.getName()).isEqualTo(this.testName);
+        Assertions.assertThat(resultByName.getName()).isNotEmpty();
     }
 }
