04a23f3cef3abc3b2cfede8ee59ca9507c388dde
maaaaaaaaad <integral5137@gmail.com>
2023-08-07 21:02:35 +0900

    e

--- src/test/java/com/mad/mad/repository/TemporaryMemberRepositoryTest.java
+++ src/test/java/com/mad/mad/repository/TemporaryMemberRepositoryTest.java
@@ -8,21 +8,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TemporaryMemberRepositoryTest {
-
     final String testName = "mad";
-    final Long testId = 1000L;
-
     MemberRepository repository = new TemporaryMemberRepository();
     Member member = new Member();
 
     @Test()
     public void save() {
-        this.member.setId(this.testId);
         this.member.setName(this.testName);
         repository.save(this.member);
-        Member resultById = repository.findByName(this.member.getName()).get();
+        Member resultById = repository.findById(this.member.getId()).get();
         Assertions.assertThat(this.member).isEqualTo(resultById);
-        System.out.print(resultById.getId());
         Assertions.assertThat(resultById.getId()).isEqualTo(1L);
         Assertions.assertThat(resultById.getId()).isNotZero();
         Member resultByName = repository.findByName(this.member.getName()).get();
