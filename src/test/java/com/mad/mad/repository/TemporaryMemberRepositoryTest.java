5616056887120bcee5b76da9002c623db2357d34
maaaaaaaaad <integral5137@gmail.com>
2023-08-08 20:40:43 +0900

    e

--- src/test/java/com/mad/mad/repository/TemporaryMemberRepositoryTest.java
+++ src/test/java/com/mad/mad/repository/TemporaryMemberRepositoryTest.java
@@ -11,35 +11,6 @@ import java.util.Optional;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class TemporaryMemberRepositoryTest {
-    final String testName = "mad";
-    MemberRepository repository = new TemporaryMemberRepository();
-    Member member = new Member();
 
-
-    @Test()
-    public void findById() {
-        Optional<Member> empty = repository.findById(1000L);
-        assertThat(empty).isEmpty();
-    }
-
-    @Test()
-    public void findByName() {
-        Optional<Member> empty = repository.findByName(this.testName);
-        assertThat(empty).isEmpty();
-    }
-
-    @Test()
-    public void save() {
-        this.member.setName(this.testName);
-        repository.save(this.member);
-        Member resultById = repository.findById(this.member.getId()).get();
-        assertThat(this.member).isEqualTo(resultById);
-        assertThat(resultById.getId()).isEqualTo(1L);
-        assertThat(resultById.getId()).isNotZero();
-        Member resultByName = repository.findByName(this.member.getName()).get();
-        assertThat(this.member).isEqualTo(resultByName);
-        assertThat(resultByName.getName()).isEqualTo(this.testName);
-        assertThat(resultByName.getName()).isNotEmpty();
-    }
 
 }
