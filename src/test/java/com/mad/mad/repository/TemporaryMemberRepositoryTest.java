420bd848dd05c8d5494da3c16ddc2828eb4366bd
maaaaaaaaad <integral5137@gmail.com>
2023-08-08 20:48:40 +0900

    e

--- src/test/java/com/mad/mad/repository/TemporaryMemberRepositoryTest.java
+++ src/test/java/com/mad/mad/repository/TemporaryMemberRepositoryTest.java
@@ -12,5 +12,16 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class TemporaryMemberRepositoryTest {
 
+    MemberRepository repository = new TemporaryMemberRepository();
+    Member member = new Member();
+
+    @Test()
+    public void save() {
+        Member result = repository.save(member);
+        long id = result.getId();
+        assertThat(id).isEqualTo(1);
+        assertThat(id).isNotZero();
+        assertThat(id).isNotNull();
+    }
 
 }
