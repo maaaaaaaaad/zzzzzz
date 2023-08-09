993093ab3f1524b0bfeaee25edab07b52d5f7253
maaaaaaaaad <integral5137@gmail.com>
2023-08-09 20:50:35 +0900

    e

--- src/test/java/com/mad/mad/repository/TemporaryMemberRepositoryTest.java
+++ src/test/java/com/mad/mad/repository/TemporaryMemberRepositoryTest.java
@@ -16,18 +16,23 @@ public class TemporaryMemberRepositoryTest {
     Member member = new Member();
 
     @Test()
+    public void findByName() {
+        final String name = member.getName();
+        Optional<Member> result = repository.findByName(name);
+        assertThat(name).isNotEqualTo(result);
+
+        final String mad = "mad";
+        member.setName(mad);
+        Member member1 = repository.save(member);
+        assertThat(mad).isEqualTo(member1.getName());
+    }
+
+    @Test()
     public void save() {
         Member result = repository.save(member);
         long id = result.getId();
         assertThat(id).isEqualTo(1);
         assertThat(id).isNotZero();
         assertThat(id).isNotNull();
-    }
-
-    @Test()
-    public void findByName() {
-        final String name = member.getName();
-        Optional<Member> result = repository.findByName(name);
-        assertThat(name).isNotEqualTo(result);
     }
 }
