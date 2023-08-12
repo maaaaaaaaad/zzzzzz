862b24195fbedb693495812f62cdd1ce97647936
maaaaaaaaad <integral5137@gmail.com>
2023-08-12 17:05:00 +0900

    e

--- src/main/java/com/mad/mad/domain/service/MemberService.java
+++ src/main/java/com/mad/mad/domain/service/MemberService.java
@@ -8,14 +8,14 @@ import java.util.Optional;
 
 public class MemberService implements IMemberService {
 
-    private final IMemberRepository repository = new TemporaryIMemberRepository();
+    private static final IMemberRepository repository = new TemporaryIMemberRepository();
 
     @Override
     public final Member join(Member member) {
-        Optional<Member> name = this.repository.findByName(member.getName());
+        Optional<Member> name = repository.findByName(member.getName());
         name.ifPresent(query -> {
             throw new Error("name");
         });
-        return this.repository.save(member);
+        return repository.save(member);
     }
 }
